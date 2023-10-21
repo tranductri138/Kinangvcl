827f87766088c08be9bf16432f89a4696ee801d6
trisdepzai <tritocao@gmail.com>
2023-10-21 20:34:28 +0700

    [21-10-2023] Update code

---
+++ thuattoan/datastructure/whatIsADatastructure.js
@@ -0,0 +1,19 @@
+/**
+ * link
+ * https://en.wikipedia.org/wiki/List_of_data_structures
+ * */
+
+/**
+ * Explain
+ * You can think of data structure as any sort of compartment or container
+ * A dtstt is simply a file cabinet of different types
+ * How to build one
+ * How to use it
+ * */
+
+/**
+ * How to computer store data
+ * link
+ * https://coggle.it/diagram/W5E5tqYlrXvFJPsq/t/master-the-interview-click-here-for-course-link/c25f98c73a03f5b1107cd0e2f4bce29c9d78e31655e55cb0b785d56f0036c9d1
+ *
+ * */
