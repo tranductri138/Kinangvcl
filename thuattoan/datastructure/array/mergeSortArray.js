5c957f09aa9e2a7ba760578b679bfd8815f0e34a
trisdepzai <tritocao@gmail.com>
2023-10-22 23:41:12 +0700

    [22-10-2023] Update code

--- thuattoan/datastructure/array/mergeSortArray.js
+++ thuattoan/datastructure/array/mergeSortArray.js
@@ -7,16 +7,18 @@ function mergeSortedArrays(arrayFirst, arraySecond){
     // should use destructuring
     let arrFirstItem = arrayFirst[0];
     let arrSecondItem = arraySecond[0];
-    let indexArrFirst = 1;
+    let indexArrFirst = 1; // value next ==> because assign and then ++
     let indexArrSecond = 1;
 
     // recursion
     while (arrFirstItem || arrSecondItem){
         // console.log(arrFirstItem ,'and' , arrSecondItem )
         if(arrSecondItem === undefined || arrFirstItem < arrSecondItem){
+            // condition array F < arr Sec of === so throw case 2
+            // if in case
             mergedArray.push(arrFirstItem);
-            arrFirstItem = arrayFirst[indexArrFirst];
-            indexArrFirst++;
+            arrFirstItem = arrayFirst[indexArrFirst]; // 1  assign
+            indexArrFirst++; // 2
         }
         else {
             mergedArray.push(arrSecondItem);
