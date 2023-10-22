67c1c8c5feaafe77ae7560c8e86022d777ed80f2
trisdepzai <tritocao@gmail.com>
2023-10-22 23:16:40 +0700

    [22-10-2023] Update code

--- test/testFunction.js
+++ test/testFunction.js
@@ -3,6 +3,7 @@ function Person() {
 
     // Sử dụng arrow function và closure để truy cập this
     this.greet = () => {
+        // this in here ==> se la this ben ngoai
         console.log(`Hello, my name is ${this.name}`);
     };
 }
