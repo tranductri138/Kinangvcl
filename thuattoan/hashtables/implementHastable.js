cb9acf91b8a0aa63c09de3a2fe719634c37ee7e1
tranductri <tritd@foobla.com>
2023-10-24 08:31:37 +0700

    [23-10-2023] Update code

--- thuattoan/hashtables/implementHastable.js
+++ thuattoan/hashtables/implementHastable.js
@@ -1,12 +1,12 @@
 class HashTable {
-    constructor(size){
+    constructor(size) {
         this.data = new Array(size);
         // this.data = [];
     }
 
     _hash(key) {
         let hash = 0;
-        for (let i =0; i < key.length; i++){
+        for (let i = 0; i < key.length; i++) {
             hash = (hash + key.charCodeAt(i) * i) % this.data.length
         }
         return hash;
@@ -21,20 +21,54 @@ class HashTable {
         return this.data;
     }
 
-    get(key){
+    get(key) {
         const address = this._hash(key);
         const currentBucket = this.data[address]
         if (currentBucket) {
-            for(let i = 0; i < currentBucket.length; i++){
-                if(currentBucket[i][0] === key) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
                     return currentBucket[i][1]
                 }
             }
         }
         return undefined;
     }
-    toString (){
+
+    toString() {
         return this.data
+    }
+
+    keys() {
+        const keysArray = [];
+        // console.log(this.data.length);
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i]) {
+                keysArray.push(this.data[i][0][0])
+            }
+        }
+        return keysArray;
+    }
+
+    keys2() {
+        if (!this.data.length) {
+            return undefined
+        }
+        let result = []
+        // loop through all the elements
+        for (let i = 0; i < this.data.length; i++) {
+            // if it's not an empty memory cell
+            if (this.data[i] && this.data[i].length) {
+                // but also loop through all the potential collisions
+                if (this.data.length > 1) {
+                    for (let j = 0; j < this.data[i].length; j++) {
+                        result.push(this.data[i][j][0])
+                    }
+                } else {
+                    result.push(this.data[i][0])
+                }
+            }
+        }
+        return result;
     }
 }
 
@@ -43,4 +77,8 @@ myHashTable.set('grapes', 10000)
 myHashTable.get('grapes')
 myHashTable.set('apples', 9)
 myHashTable.get('apples')
-console.log(myHashTable.toString())
+console.log(myHashTable.keys())
+console.log(myHashTable.keys2())
+// console.log(myHashTable.toString())
+
+// can not access frequent all hash table
