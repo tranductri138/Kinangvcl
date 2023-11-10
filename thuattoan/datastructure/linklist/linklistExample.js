0fa5ef0314bd7ca6f11533061b757b1e73aceb2b
tranductri <tritd@foobla.com>
2023-11-10 16:14:13 +0700

    [10-11-23] Update code

--- thuattoan/datastructure/linklist/linklistExample.js
+++ thuattoan/datastructure/linklist/linklistExample.js
@@ -9,3 +9,25 @@ const basket = ['apples', 'grapes', 'pears']
 
 // head node tail null
 // pointter
+
+
+/**
+ * Compare single link list with double link list
+ * single chi co node va co 2 cai de tien vs ghi node
+ * con double se can them node prev nua vay nen no se ton nhieu bo nho hon
+ * almost se hoi ve single
+ * */
+
+/**
+ * Link list
+ * fast insert
+ * fast delete
+ * ordered
+ * flexible sz
+ *
+ * but
+ * slow lookup
+ * more memory
+ * */
+
+
