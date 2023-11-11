ca33064c71205974b5df859e5d3862eb6f41ff91
trisdepzai <tritocao@gmail.com>
2023-11-12 01:34:36 +0700

    [12-11-2023] Update code

--- thuattoan/leetcode/linkedlist/MergeTwoSortedLists.js
+++ thuattoan/leetcode/linkedlist/MergeTwoSortedLists.js
@@ -8,17 +8,18 @@
  * description
  * merge the fist and the second and then sort
  */
+
+
+function ListNode(val = undefined, next = undefined) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
 /**
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
  */
-
-function ListNode(val, next = undefined) {
-    this.val = (val === undefined ? 0 : val)
-    this.next = (next === undefined ? null : next)
-}
-
 function mergeTwoLists(list1, list2) {
     let dummy = new ListNode(0)
     let head = dummy
@@ -41,6 +42,30 @@ function mergeTwoLists(list1, list2) {
     }
     return head.next
 };
+
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+function mergeTwoLists2(list1, list2) {
+    // valid input
+    if (!list1) return list2
+    else if (!list2) return list1
+    else if(!list1 && !list2) return  new ListNode()
+
+    // condition
+    else if (list1.val <= list2.val){
+        list1.next = mergeTwoLists2(list1.next, list2);
+        return list1;
+    } else{
+        list2.next = mergeTwoLists2(list1, list2.next);
+        return list2;
+    }
+};
+
+
 // 1n2 24 13 34
 const nodeFirst3 = new ListNode(4)
 const nodeFirst2 = new ListNode(2, nodeFirst3)
@@ -49,7 +74,7 @@ const list1 = new ListNode(1, nodeFirst2)
 const nodeSecond3 = new ListNode(4)
 const nodeSecond2 = new ListNode(3, nodeSecond3)
 const list2 = new ListNode(1, nodeSecond2)
-const sortList = mergeTwoLists(list1 , list2)
+const sortList = mergeTwoLists2(list1 , list2)
 
 
 // console.log( 'this is list1: ',JSON.stringify(list1))
