cd34fafa5b8ba6452525a5f544f9cca0ab9d92f9
tranductri <tritd@foobla.com>
2023-11-14 17:31:26 +0700

    [14-11-23] Update code

--- designpattern/builder/baitapCoding.js
+++ designpattern/builder/baitapCoding.js
@@ -52,22 +52,22 @@ class CodeBuilder {
     }
 }
 
-describe('builder', function () {
-    it('empty test', function () {
-        let cb = new CodeBuilder('Foo');
-        expect(cb.toString())
-            .toEqual('class Foo {\n}');
-    });
-
-    it('person test', function () {
-        let cb = new CodeBuilder('Person');
-        cb.addField('name').addField('age');
-        expect(cb.toString())
-            .toEqual('class Person {\n'
-                + '  constructor(name, age) {\n'
-                + '    this.name = name;\n'
-                + '    this.age = age;\n'
-                + '  }\n'
-                + '}');
-    });
-});
+// describe('builder', function () {
+//     it('empty test', function () {
+//         let cb = new CodeBuilder('Foo');
+//         expect(cb.toString())
+//             .toEqual('class Foo {\n}');
+//     });
+//
+//     it('person test', function () {
+//         let cb = new CodeBuilder('Person');
+//         cb.addField('name').addField('age');
+//         expect(cb.toString())
+//             .toEqual('class Person {\n'
+//                 + '  constructor(name, age) {\n'
+//                 + '    this.name = name;\n'
+//                 + '    this.age = age;\n'
+//                 + '  }\n'
+//                 + '}');
+//     });
+// });
