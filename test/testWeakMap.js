779ee56c95420205f43a6f5d710a700dd41e87c3
tranductri <tritd@foobla.com>
2023-11-14 08:09:21 +0700

    [14-11-23] Update code

--- test/testWeakMap.js
+++ test/testWeakMap.js
@@ -6,6 +6,29 @@ function test1() {
     const a = new Map()
 
     a.set('asd' , 'as')
+    a.set('c' , {a: 1 , b :2})
     console.log(a.get('asd'))
+    console.log(a.get('c'))
 }
-test1()
+// test1()
+
+function test2() {
+    const a = [{ a :1} , { a :1}, { a :1}]
+    const b = [{b:2}]
+    b.push(...a)
+    console.log(b)
+}
+// test2()
+
+function test3() {
+    const a = [1,2,3,4,5]
+
+    for (const number of a) {
+        console.log('here')
+        if (number === 3) {
+            break
+        }
+    }
+}
+
+test3()
