7fb08b4f019a0c38dbc250d2e59c68a423828f35
tranductri <tritd@foobla.com>
2024-01-31 10:13:56 +0700

    [31-1-2023] Update code

---
+++ thuattoan/backTracking/Combinations.js
@@ -0,0 +1,15 @@
+/**
+ * link: https://leetcode.com/problems/combinations/description/
+ * description: Given two integers n and k, return all possible
+ * combinations of k numbers chosen from the range [1, n].
+ * You may return the answer in any order.
+ * */
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+const combine = (n, k) => {
+
+}
