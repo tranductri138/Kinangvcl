2d3581d1c69e801fa4487c8d200481ea24add2a0
tranductri <tritd@foobla.com>
2024-03-21 10:56:23 +0700

    [21-3-2023] Update code

--- designpattern/singleton/singletonProblem.test.js
+++ designpattern/singleton/singletonProblem.test.js
@@ -81,27 +81,34 @@ class DummyDatabase
 }
 
 
-describe('singleton database', function()
-{
-    it('is a singleton', function()
-    {
-        const db1 = new MyDatabase();
-        const db2 = new MyDatabase();
-        expect(db1).toBe(db2);
-    });
+const test = () => {
+    const db1 = new MyDatabase();
+    const db2 = new MyDatabase();
+    console.log(db1 === db2)
+}
 
-    it('calculates total population', function()
-    {
-        let rf = new SingletonRecordFinder();
-        let cities = ['Seoul', 'Mexico City'];
-        let tp = rf.totalPopulation(cities);
-        expect(tp).toEqual(17400000+17500000);
-    });
+test()
 
-    it('calculates total population better', function()
-    {
-        let db = new DummyDatabase();
-        let rf = new ConfigurableRecordFinder(db);
-        expect(rf.totalPopulation(['alpha', 'gamma'])).toEqual(4);
-    });
-});
+// describe('singleton database', function()
+// {
+//     it('is a singleton', function()
+//     {
+//
+//         expect(db1).toBe(db2);
+//     });
+//
+//     it('calculates total population', function()
+//     {
+//         let rf = new SingletonRecordFinder();
+//         let cities = ['Seoul', 'Mexico City'];
+//         let tp = rf.totalPopulation(cities);
+//         expect(tp).toEqual(17400000+17500000);
+//     });
+//
+//     it('calculates total population better', function()
+//     {
+//         let db = new DummyDatabase();
+//         let rf = new ConfigurableRecordFinder(db);
+//         expect(rf.totalPopulation(['alpha', 'gamma'])).toEqual(4);
+//     });
+// });
--- solid/singleResponsibility.js
+++ solid/singleResponsibility.js
@@ -1,56 +1,53 @@
 const fs = require('fs');
+const path = require("path");
 
-class Journal
-{
+class Journal {
     constructor() {
         this.entries = {};
     }
 
-    addEntry(text)
-    {
-        let c = ++Journal.count;
-        let entry = `${c}: ${text}`;
+    addEntry(text) {
+        let c = ++ Journal.count;
+        let entry = `${ c }: ${ text }`;
         this.entries[c] = entry;
         return c;
     }
 
-    removeEntry(index)
-    {
+    removeEntry(index) {
         delete this.entries[index];
     }
 
-    toString()
-    {
+    toString() {
         return Object.values(this.entries).join('\n');
     }
 
-    // save(filename)
-    // {
-    //   fs.writeFileSync(filename, this.toString());
-    // }
-    //
-    // load(filename)
-    // {
-    //   //
-    // }
-    //
-    // loadFromUrl(url)
-    // {
-    //   //
-    // }
-}
-Journal.count = 0;
+    save(filename) {
+        fs.writeFileSync(filename, this.toString());
+    }
 
-class PersistenceManager
-{
-    preprocess(j)
-    {
+    load(filename) {
         //
     }
 
-    saveToFile(journal, filename)
-    {
-        fs.writeFileSync(filename, journal.toString());
+    loadFromUrl(url) {
+        //
+    }
+}
+
+Journal.count = 0;
+
+class PersistenceManager {
+    constructor(filename, journal) {
+        this.filename = filename
+        this.journal = journal
+    }
+
+    preProcess () {
+        console.log('do some thing')
+    }
+
+    saveToFile() {
+        fs.writeFileSync(this.filename, this.journal.toString());
     }
 }
 
@@ -59,8 +56,9 @@ j.addEntry('I cried today.');
 j.addEntry('I ate a bug.');
 console.log(j.toString());
 
-let p =new PersistenceManager();
-let filename = 'c:/temp/journal.txt';
-p.saveToFile(j, filename);
+let filename = path.join(__dirname, 'jText')
+let p = new PersistenceManager(filename , j);
+p.preProcess()
+p.saveToFile();
 
 // separation of concerns
